1ab7c8dfc07de8ffe622eb286aec4ceb72c196df
MilenaMed <milenamfm@gmail.com>
2022-11-03 23:26:10 -0300

    dinamização da Tela3.1
    Tela 3.1 seguindo os requisitos do projeto e mudando para 3.2 após o cumprimento de todo os requisitos

--- script.js
+++ script.js
@@ -40,3 +40,37 @@ function renderQuizz(res){
 }
 
 /*________________________________________TELA3________________________________________*/
+
+// Criação do Quizz
+
+function confirmarCriação(confirmar){
+    let texto = document.querySelector("#Titulo");
+    let imagURL = document.querySelector("#imgURL");
+    let qtddPerg = document.querySelector("#qtdPerg");
+    let qtddNiveis = document.querySelector("#qtdNiveis");
+    let nPerguntas = parseInt(qtddPerg.value)
+    let nNiveis = parseInt(qtddNiveis.value)
+
+    try {
+        url = new URL(imagURL.value)
+      } catch(err) {
+        url = false;
+      }
+    
+     if( 20 <= texto.value.length && texto.value.length < 65 && nPerguntas >= 3 && nNiveis >= 2 && false !== url){
+
+        const novoQuizz = {
+                    title: texto.value,
+            	    image: imagURL.value,}
+
+        const $divContainerTela31 = document.querySelector('.conteinerTela31');
+        $divContainerTela31.classList.add('desativar');
+        const $divContainerTela32 = document.querySelector('.conteinerTela32');
+        $divContainerTela32.classList.remove('desativar');
+
+    } else {
+    alert("Por favor preencha os dados corretamente")
+    }
+}
+
+// Criar perguntas
