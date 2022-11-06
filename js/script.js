2a533832f77a3794e32bb17f2865d735e6dc071b
MilenaMed <milenamfm@gmail.com>
2022-11-06 14:04:29 -0300

    Corrigindo questão dos requisitos das perguntas
    Add questão da imagem e pergunta incorreta respondida

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ getAPI();
 function getAPI(){
     const promisse = axios.get(`${urlAPI}`);
     promisse.then(renderQuizzes);
-    
+
 }
 
 function createQuizz(){
@@ -74,20 +74,14 @@ for(indice= 0 ; indice < quizz.questions.length ; indice++){
 
      const container = document.querySelector(".container")    
      container.innerHTML += 
-    `
-            
+    `   
             <div class="caixaPergunta">  
-
                 <div class="cabecalhoPergunta">
-
                     <span class="tituloPergunta">${quizz.questions[indice].title}</span>
-
                 </div>
-
                 <div class="caixarespostas">
                 </div> 
-                
-
+               
             </div>    
         
     `                
@@ -108,7 +102,6 @@ for (let i=0;i<quizz.questions.answers.length;i++){
         caixaRespostas.innerHTML +=
         
         `
-
         <div class="resposta">
             <img class="imgResposta" src="${quizz.questions[indice].answers[i].image}">
             <span class="textoResposta">src="${quizz.questions[indice].answers[i].text}</span> 
@@ -150,119 +143,158 @@ function ReiniciarQuiz(){
 }
 
 /*________________________________________TELA3________________________________________*/
+/* Criar perguntas */
 
-// Criação do Quizz
-
-function confirmarCriação(confirmar){
-    // let texto = document.querySelector("#Titulo");
-    // let imagURL = document.querySelector("#imgURL");
-    let qtddPerg = document.querySelector("#qtdPerg");
-    // let qtddNiveis = document.querySelector("#qtdNiveis");
-    let nPerguntas = parseInt(qtddPerg.value)
-    // let nNiveis = parseInt(qtddNiveis.value)
-
-    // try {
-    //     url = new URL(imagURL.value)
-    //   } catch(err) {
-    //     url = false;
-    //   }
-    
-    //  if( 20 <= texto.value.length && texto.value.length < 65 && nPerguntas >= 3 && nNiveis >= 2 && false !== url){
-
-    //     const novoQuizz = {
-    //                 title: texto.value,
-    //         	    image: imagURL.value,}
-
-        const $divContainerTela31 = document.querySelector('.conteinerTela31');
-        $divContainerTela31.classList.add('desativar');
-        const $divContainerTela32 = document.querySelector('.conteinerTela32');
-        $divContainerTela32.classList.remove('desativar');
-
-        criarPerguntas()
-
-    // } else {
-    // alert("Por favor preencha os dados corretamente")
-    // }
-}
-
-// Criar perguntas
-
-function criarPerguntas(){
+function criarPerguntas() {
     let qtddPerg = document.querySelector("#qtdPerg");
     let nPerguntas = parseInt(qtddPerg.value);
     const perguntasQ = document.querySelector('.conteinerTela32');
-    for(let i = 1; i <= nPerguntas; i++){
+    for (let i = 1; i <= nPerguntas; i++) {
         let templatePergFch = `
+        <div class="conteinerperguntas" onclick="abrirPerguntas(this)">
         <div class="perguntasEditaveis">
             <p class="perguntaNum">Pergunta ${i}</p>
-           <ion-icon onclick="abrirPerguntas(this)" name="create-outline" class="icone"></ion-icon>
+           <ion-icon name="create-outline" class="icone"></ion-icon>
          </div>
          <div class="perguntasAbertas desativar">
         <div class="duplaCaixas">
             Pergunta ${i}
-            <input type="text" size="35" value=" Texto da pergunta" id="textoPerg" class="caixaDigitavel" />
-           <input type="text" size="35" value=" Cor de fundo" id="cor" class="caixaDigitavel" />
-        </div>
+            <input type="text" size="35" placeholder=" Texto da pergunta" id="textoPerg" class="caixaDigitavel" />
+           <input type="text" size="35" placeholder=" Cor de fundo" id="cor" class="caixaDigitavel" />
+           </div>
        <div class="duplaCaixas">
              Resposta 
-           <input type="text" size="35" value=" Resposta correta" id="respCorreta" class="caixaDigitavel" />
-           <input type="text" size="35" value=" URL da imagem" id="UrlImagCorreta" class="caixaDigitavel" />
+           <input type="text" size="35" placeholder=" Resposta correta" id="respCorreta" class="caixaDigitavel" />
+           <input type="text" size="35" placeholder=" URL da imagem" id="UrlImagCorreta" class="caixaDigitavel" />
          </div>
         <div class="duplaCaixas">
         Respostas incorretas
-             <input type="text" size="35" value=" Resposta incorreta 1" id="incorreta1" class="caixaDigitavel" />
-             <input type="text" size="35" value=" URL da imagem 1" id="imgIncorreta1" class="caixaDigitavel" />
+             <input type="text" size="35" placeholder=" Resposta incorreta 1" id="incorreta1" class="caixaDigitavel" />
+             <input type="text" size="35" placeholder=" URL da imagem 1" id="imgURL1" class="caixaDigitavel" />
         </div>
         <div class="duplaCaixas">
-            <input type="text" size="35" value=" Resposta incorreta 2" id="incorreta2" class="caixaDigitavel" />
-            <input type="text" size="35" value=" URL da imagem 2" id="imgIncorreta2" class="caixaDigitavel" />
+            <input type="text" size="35" placeholder=" Resposta incorreta 2" id="incorreta2" class="caixaDigitavel" />
+            <input type="text" size="35" placeholder=" URL da imagem 2" id="imgURL2" class="caixaDigitavel" />
         </div>
         <div class="duplaCaixas">
-            <input type="text" size="35" value=" Resposta incorreta 3" id="incorreta3" class="caixaDigitavel" />
-            <input type="text" size="35" value=" URL da imagem 3" id="imgIncorreta3" class="caixaDigitavel" />
+            <input type="text" size="35" placeholder=" Resposta incorreta 3" id="incorreta3" class="caixaDigitavel" />
+            <input type="text" size="35" placeholder=" URL da imagem 3" id="imgURL3" class="caixaDigitavel" />
         </div>
     </div>
+    </div>
         `;
-        
+
         perguntasQ.innerHTML += templatePergFch;
     }
     const irParaNiveis = document.querySelector('.conteinerTela32');
-    let habilitarBotão = 
-    `
+    let habilitarBotão =
+        `
         <button onclick="requisitosPerguntas(this)" class="prosseguirNiveis">
             Prosseguir para níveis
         </button>`;
-        
-        irParaNiveis.innerHTML += habilitarBotão;
+
+    irParaNiveis.innerHTML += habilitarBotão;
 }
 
-function abrirPerguntas(){
-    const $perguntasEditaveis = document.querySelector('.perguntasEditaveis');
-        $perguntasEditaveis.classList.add('desativar');
-        const $perguntasAbertas = document.querySelector('.perguntasAbertas');
-        $perguntasAbertas.classList.remove('desativar');
+function abrirPerguntas($divCompleta) {
+    const $perguntasEditaveis = $divCompleta.querySelector('.perguntasEditaveis');
+    $perguntasEditaveis.classList.add('desativar');
 
-//  TEM QUE ABRIR A PERGUNTA CLICAADA
+    const $perguntasAbertas = $divCompleta.querySelector('.perguntasAbertas');
+    $perguntasAbertas.classList.remove('desativar');
 }
 
-function requisitosPerguntas(){
+function requisitosPerguntas($divCompleta) {
     let textoPerg = document.querySelector("#textoPerg");
     let PergCor = document.querySelector("#cor");
     let RespIncorreta1 = document.querySelector("#incorreta1");
-    // let validos = ["a","b","c","d","e","f","1","2","3","4","6","5"];
-    // let i = indice;
+    let RespIncorreta2 = document.querySelector("#incorreta2");
+    let RespIncorreta3 = document.querySelector("#incorreta3");
+    let ResCorreta = document.querySelector('#respCorreta');
+    let imagURLIncorreta1 = document.querySelector("#imgURL1");
+    let imagURLIncorreta2 = document.querySelector("#imgURL2");
+    let imagURLIncorreta3 = document.querySelector("#imgURL3");
+    let imagURLC = document.querySelector("#UrlImagCorreta");
 
-    // conferir se a cor só tem esses dados válidos
-    // LIMPAR CAIXA DE TEXTO AO CLICAR NELA
+    // conferir se a cor só tem dados válidos
+    try {
+        let RegExp = /^#[0-9A-F]{6}$/i;
+        ehHexa = RegExp.test(PergCor.value)
+    } catch (err) {
+        ehHexa = false
+    }
+    console.log(ehHexa)
+    // conferir se a url da imagem correta é válido
+    try {
+        urlc = new URL(imagURLC.value)
+    } catch (err) {
+        urlc = false;
+    }
 
-    if( 20 <= textoPerg.value.length && PergCor.value[0] == "#" && PergCor.value.length == 7 && RespIncorreta1.value.length !== 0){
-        console.log(textoPerg.value)
-        console.log(PergCor.value)
-    }else{
+    //   console.log (RespIncorreta3.value)
+    if (20 <= textoPerg.value.length && ResCorreta.value.length > 1 && urlc !== false && ehHexa !== false && (RespIncorreta1.value.length > 1 || RespIncorreta2.value.length > 1 || RespIncorreta3.value.length > 1)) {
+        if (
+            conferirImagemRespondida(RespIncorreta1, imagURLIncorreta1) &&
+            conferirImagemRespondida(RespIncorreta2, imagURLIncorreta2) &&
+            conferirImagemRespondida(RespIncorreta3, imagURLIncorreta3)
+        ) {
+            perguntasNosRequisitos(true)
+
+        }
+        else {
+            alert("Por favor preencha os dados corretamente")
+        }
+    } else {
          alert("Por favor preencha os dados corretamente")
-         }
+    }
+}
+
+function conferirImagemRespondida(resposta, imagem) {
+    // conferir se a url da imagem incorreta é válido
+    try {
+        url1 = new URL(imagem.value)
+    } catch (err) {
+        url1 = false;
+    }
+    if ((resposta.value.length > 1 && url1 !== false) || (resposta.value.length == 0 && imagem.value.length == 0)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function perguntasNosRequisitos(){
+    console.log("oi")
+    // const $perguntasEditaveis = $divCompleta.querySelector('.perguntasEditaveis');
+    // $perguntasEditaveis.classList.add('desativar');
+
+    // const $perguntasAbertas = $divCompleta.querySelector('.perguntasAbertas');
+    // $perguntasAbertas.classList.remove('desativar')
 
 }
-// ao cliclar no editavel abrir perguntas
-// seguir requisitos do trello
-// só passar para níveis com perguntas todas cumprindo os requisitos
+
+/* --------Criação de niveis--------*/
+
+function openLevels(level) {
+
+    let numLevel = document.querySelector('.nivelNum').innerHTML.replace("Nivel", "");
+
+    let layout = `
+    <div class="conteinerNiveis">
+        Nivel${numLevel}
+        <input type="text" size="35" placeholder=" Título do nível${numLevel}" class="caixaDigitavel" />
+        <input type="text" size="35" placeholder=" % de acerto mínima" class="caixaDigitavel" />
+        <input type="text" size="35" placeholder=" URL da imagem nível${numLevel}" class="caixaDigitavel" />
+        <input type="text" size="35" placeholder=" Descrição do nível${numLevel}" class="caixaDigitavel grande" />
+    </div>`
+
+    if (level.innerHTML !== layout) {
+        level.innerHTML = "";
+        level.innerHTML += layout;
+        level.removeAttribute('onclick');
+    }
+}
+
+function requestLevels() {
+
+}
