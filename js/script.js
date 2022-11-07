6e87e6ddf1917a19b406f2324d6f59f87a8e6f3e
MilenaMed <milenamfm@gmail.com>
2022-11-07 01:34:00 -0300

    Add requisitos de todas as perguntas respondidas e inicio da criação do objeto de envio

--- js/script.js
+++ js/script.js
@@ -7,32 +7,32 @@ const tela2 = document.querySelector('.tela2');
 
 getAPI();
 
-function getAPI(){
+function getAPI() {
     const promisse = axios.get(`${urlAPI}`);
     promisse.then(renderQuizzes);
 
 }
 
-function createQuizz(){
+function createQuizz() {
     window.location = "tela3.html";
 }
 
-function changeLayout(){
+function changeLayout() {
     const myQuizzes = document.querySelector('.myQuizzes');
     const ulMyQuizzes = document.querySelector('.ulMyQuizzes');
     const myQuizzesEmpty = document.querySelector('.myQuizzesEmpty');
 
-    if(ulMyQuizzes.innerHTML !== null){
+    if (ulMyQuizzes.innerHTML !== null) {
         myQuizzes.classList.remove('hidden');
         myQuizzesEmpty.classList.add('hidden');
     }
 }
 
-function renderQuizzes(res){
+function renderQuizzes(res) {
     const liQuizzes = res.data;
     const ulQuizzes = document.querySelector('.quizzes');
 
-    for(let i = 0; i < liQuizzes.length; i++){
+    for (let i = 0; i < liQuizzes.length; i++) {
         let quizz = liQuizzes[i];
         let layout = `
         <li onclick="playQuizz(${quizz.id})" class="quizz">
@@ -44,7 +44,7 @@ function renderQuizzes(res){
     }
 }
 
-function playQuizz(id){
+function playQuizz(id) {
     const promisse = axios.get(`${urlAPI}/${id}`);
     promisse.then(renderQuizz);
     //console.log(id)
@@ -52,15 +52,15 @@ function playQuizz(id){
 
 /*________________________________________TELA2________________________________________*/
 
-function renderQuizz(res){
+function renderQuizz(res) {
     tela1.classList.add('hidden');
     tela2.classList.remove('hidden');
     const quizz = res.data;
-    
+
     //Coloca o cabeçalho do quizz / imagem e título   
     const cabecalho = document.querySelector(".cabecalhoQuizz")
-    cabecalho.innerHTML +=  
-    `                            
+    cabecalho.innerHTML +=
+        `                            
             <div class="gradient">
                 <h2>${quizz.title}</h2>
             </div>
@@ -68,13 +68,13 @@ function renderQuizz(res){
     `
 
 
-//Cria a quantidade de caixas de perguntas e coloca o título 
-for(indice= 0 ; indice < quizz.questions.length ; indice++){  
+    //Cria a quantidade de caixas de perguntas e coloca o título 
+    for (indice = 0; indice < quizz.questions.length; indice++) {
 
 
-     const container = document.querySelector(".container")    
-     container.innerHTML += 
-    `   
+        const container = document.querySelector(".container")
+        container.innerHTML +=
+            `   
             <div class="caixaPergunta">  
                 <div class="cabecalhoPergunta">
                     <span class="tituloPergunta">${quizz.questions[indice].title}</span>
@@ -84,24 +84,25 @@ for(indice= 0 ; indice < quizz.questions.length ; indice++){
                
             </div>    
         
-    `                
-}
+    `
+    }
 
-//console.log(quizz.questions[0].answers)
+    //console.log(quizz.questions[0].answers)
 
 
-//função para misturar as respostas
-function comparador() { 
-    return (Math.random() - 0.5) }
+    //função para misturar as respostas
+    function comparador() {
+        return (Math.random() - 0.5)
+    }
 
-    
-//Gera as respostas das perguntas sortidas
-for (let i=0;i<quizz.questions.answers.length;i++){
+
+    //Gera as respostas das perguntas sortidas
+    for (let i = 0; i < quizz.questions.answers.length; i++) {
 
         const caixaRespostas = document.querySelector(".caixarespostas")
         caixaRespostas.innerHTML +=
-        
-        `
+
+            `
         <div class="resposta">
             <img class="imgResposta" src="${quizz.questions[indice].answers[i].image}">
             <span class="textoResposta">src="${quizz.questions[indice].answers[i].text}</span> 
@@ -109,40 +110,79 @@ for (let i=0;i<quizz.questions.answers.length;i++){
         
         
         `
-}
+    }
 
 }
 
 //Gera o resultado
-function GerarResultado(){
+function GerarResultado() {
 
-   //Gera o Título   
-//const tituloResultado = document.querySelector(".tituloResultado")
-//tituloResultado.innerHTML += "" ;
-//tituloResultado.innerHTML += ${};
+    //Gera o Título   
+    //const tituloResultado = document.querySelector(".tituloResultado")
+    //tituloResultado.innerHTML += "" ;
+    //tituloResultado.innerHTML += ${};
 
-   //Gera a Imagem   
-//const ImagemResultado = document.querySelector(".ResultadoFinalImagem")
-//ImagemResultado.innerHTML += "" ;
-//ImagemResultado.innerHTML += ${};
+    //Gera a Imagem   
+    //const ImagemResultado = document.querySelector(".ResultadoFinalImagem")
+    //ImagemResultado.innerHTML += "" ;
+    //ImagemResultado.innerHTML += ${};
 
-   //Gera o Texto  
-//const TextoResultado = document.querySelector(".FinalTexto")
-//TextoResultado.innerHTML += "" ;
-//TextoResultado.innerHTML += ${};
+    //Gera o Texto  
+    //const TextoResultado = document.querySelector(".FinalTexto")
+    //TextoResultado.innerHTML += "" ;
+    //TextoResultado.innerHTML += ${};
 
 }
 
-function VoltarHome2(){
+function VoltarHome2() {
     window.location = "index.html";
 }
 
-function ReiniciarQuiz(){
-    
-    
+function ReiniciarQuiz() {
+
+
 }
 
 /*________________________________________TELA3________________________________________*/
+
+// Objeto a ser enviado
+
+let ArrayObejtoParaEnviar = [];
+
+// Criação do Quizz
+
+function confirmarCriação(confirmar) {
+    let texto = document.querySelector("#Titulo");
+    let imagURL = document.querySelector("#imgURL");
+    let qtddPerg = document.querySelector("#qtdPerg");
+    let qtddNiveis = document.querySelector("#qtdNiveis");
+    let nPerguntas = parseInt(qtddPerg.value)
+    let nNiveis = parseInt(qtddNiveis.value)
+
+    try {
+        url = new URL(imagURL.value)
+    } catch (err) {
+        url = false;
+    }
+
+    if (20 <= texto.value.length && texto.value.length < 65 && nPerguntas >= 3 && nNiveis >= 2 && false !== url) {
+
+        let objeto = {titulo: texto.value, image: imagURL.value }
+        ArrayObejtoParaEnviar.push(objeto)
+
+        const $divContainerTela31 = document.querySelector('.conteinerTela31');
+        $divContainerTela31.classList.add('desativar');
+        const $divContainerTela32 = document.querySelector('.conteinerTela32');
+        $divContainerTela32.classList.remove('desativar');
+
+        criarPerguntas()
+
+    } else {
+        alert("Por favor preencha os dados corretamente")
+    }
+}
+
+
 /* Criar perguntas */
 
 function criarPerguntas() {
@@ -205,16 +245,19 @@ function abrirPerguntas($divCompleta) {
 }
 
 function requisitosPerguntas($divCompleta) {
-    let textoPerg = document.querySelector("#textoPerg");
-    let PergCor = document.querySelector("#cor");
-    let RespIncorreta1 = document.querySelector("#incorreta1");
-    let RespIncorreta2 = document.querySelector("#incorreta2");
-    let RespIncorreta3 = document.querySelector("#incorreta3");
-    let ResCorreta = document.querySelector('#respCorreta');
-    let imagURLIncorreta1 = document.querySelector("#imgURL1");
-    let imagURLIncorreta2 = document.querySelector("#imgURL2");
-    let imagURLIncorreta3 = document.querySelector("#imgURL3");
-    let imagURLC = document.querySelector("#UrlImagCorreta");
+    let textoPerg = document.querySelectorAll("#textoPerg");
+    let PergCor = document.querySelectorAll("#cor");
+    let RespIncorreta1 = document.querySelectorAll("#incorreta1");
+    let RespIncorreta2 = document.querySelectorAll("#incorreta2");
+    let RespIncorreta3 = document.querySelectorAll("#incorreta3");
+    let ResCorreta = document.querySelectorAll('#respCorreta');
+    let imagURLIncorreta1 = document.querySelectorAll("#imgURL1");
+    let imagURLIncorreta2 = document.querySelectorAll("#imgURL2");
+    let imagURLIncorreta3 = document.querySelectorAll("#imgURL3");
+    let imagURLC = document.querySelectorAll("#UrlImagCorreta");
+
+    let qtddPerg = document.querySelector("#qtdPerg");
+    let nPerguntas = parseInt(qtddPerg.value);
 
     // conferir se a cor só tem dados válidos
     try {
@@ -223,7 +266,6 @@ function requisitosPerguntas($divCompleta) {
     } catch (err) {
         ehHexa = false
     }
-    console.log(ehHexa)
     // conferir se a url da imagem correta é válido
     try {
         urlc = new URL(imagURLC.value)
@@ -231,22 +273,43 @@ function requisitosPerguntas($divCompleta) {
         urlc = false;
     }
 
-    //   console.log (RespIncorreta3.value)
-    if (20 <= textoPerg.value.length && ResCorreta.value.length > 1 && urlc !== false && ehHexa !== false && (RespIncorreta1.value.length > 1 || RespIncorreta2.value.length > 1 || RespIncorreta3.value.length > 1)) {
-        if (
-            conferirImagemRespondida(RespIncorreta1, imagURLIncorreta1) &&
-            conferirImagemRespondida(RespIncorreta2, imagURLIncorreta2) &&
-            conferirImagemRespondida(RespIncorreta3, imagURLIncorreta3)
-        ) {
-            perguntasNosRequisitos(true)
+    let deucerto = 0;
+    let objetoPergunta = {};
 
+    for (let i = 0; i < nPerguntas; i++) {
+        if (20 <= textoPerg[i].value.length && ResCorreta[i].value.length > 1 && urlc[i] !== false && ehHexa[i] !== false && (RespIncorreta1[i].value.length > 1 || RespIncorreta2[i].value.length > 1 || RespIncorreta3[i].value.length > 1)) {
+            if (
+                conferirImagemRespondida(RespIncorreta1[i], imagURLIncorreta1[i]) &&
+                conferirImagemRespondida(RespIncorreta2[i], imagURLIncorreta2[i]) &&
+                conferirImagemRespondida(RespIncorreta3[i], imagURLIncorreta3[i])
+            ) {
+                deucerto += 1
+                objetoPergunta = {title: textoPerg[i].value,
+				color: PergCor[i].value ,
+				answers:[]
+            };
+            }
         }
-        else {
-            alert("Por favor preencha os dados corretamente")
-        }
-    } else {
-         alert("Por favor preencha os dados corretamente")
     }
+    if (deucerto === nPerguntas) {
+        perguntasNosRequisitos(true)
+
+        let qtdObjetos = ArrayObejtoParaEnviar.length;
+        let meuUltimoObjeto = ArrayObejtoParaEnviar.pop();
+        let objetoAtualizado = {...meuUltimoObjeto, id:qtdObjetos, questions:[...objetoPergunta]}
+        ArrayObejtoParaEnviar.push(objetoAtualizado)
+
+    } else {
+        alert("Por favor preencha os dados corretamente")
+    }
+}
+
+function abrirPerguntas($divCompleta) {
+    const $perguntasEditaveis = $divCompleta.querySelector('.perguntasEditaveis');
+    $perguntasEditaveis.classList.add('desativar');
+
+    const $perguntasAbertas = $divCompleta.querySelector('.perguntasAbertas');
+    $perguntasAbertas.classList.remove('desativar');
 }
 
 function conferirImagemRespondida(resposta, imagem) {
@@ -263,13 +326,13 @@ function conferirImagemRespondida(resposta, imagem) {
     }
 }
 
-function perguntasNosRequisitos(){
-    console.log("oi")
-    // const $perguntasEditaveis = $divCompleta.querySelector('.perguntasEditaveis');
-    // $perguntasEditaveis.classList.add('desativar');
-
-    // const $perguntasAbertas = $divCompleta.querySelector('.perguntasAbertas');
-    // $perguntasAbertas.classList.remove('desativar')
+function perguntasNosRequisitos() {
+    const $divContainerTela32 = document.querySelector('.conteinerTela32');
+    $divContainerTela32.classList.add('desativar');
+    const $divContainerTela33 = document.querySelector('.conteinerTela33');
+    $divContainerTela33.classList.remove('desativar');
+console.log(ArrayObejtoParaEnviar)
+    openLevels(level)
 
 }
 
@@ -282,18 +345,18 @@ let nNiveis = 3;
 
 addLevels()
 //Função que adiciona a quantidade de niveis escolhidos pelo usuario
-function addLevels(){
+function addLevels() {
 
     const telaNiveis = document.querySelector('.levels');
 
-    for(let i = 0; i < nNiveis; i++){
+    for (let i = 0; i < nNiveis; i++) {
         let layout = `
-        <div onclick="openLevel${i+1}(this)" class="NivelEditaveis">
-            <p class="nivelNum">Nivel ${i+1}</p>
+        <div onclick="openLevel${i + 1}(this)" class="NivelEditaveis">
+            <p class="nivelNum">Nivel ${i + 1}</p>
             <ion-icon name="create-outline" class="icone"></ion-icon>
         </div>
     `
-    telaNiveis.innerHTML += layout;
+        telaNiveis.innerHTML += layout;
     }
 
     telaNiveis.innerHTML += `
@@ -305,9 +368,9 @@ function addLevels(){
 }
 
 //funções que ao clicar adiciona os inputs
-function openLevel1(level){
+function openLevel1(level) {
 
-    let numLevel = document.querySelector('.nivelNum').innerHTML.replace("Nivel","");
+    let numLevel = document.querySelector('.nivelNum').innerHTML.replace("Nivel", "");
 
     let layout = `
     <div class="conteinerNiveis nivel1">
@@ -318,17 +381,17 @@ function openLevel1(level){
         <input id="descriptionLevel" type="text" size="35" placeholder=" Descrição do nível${numLevel}" class="caixaDigitavel grande" />
     </div>`
 
-    if(level.innerHTML !== layout){
+    if (level.innerHTML !== layout) {
         level.innerHTML = "";
         level.innerHTML += layout;
         level.removeAttribute('onclick');
     }
 }
 
-function openLevel2(level){
+function openLevel2(level) {
 
-    let numLevel = document.querySelector('.nivelNum').innerHTML.replace("Nivel","");
-    
+    let numLevel = document.querySelector('.nivelNum').innerHTML.replace("Nivel", "");
+
     let layout = `
     <div class="conteinerNiveis nivel2">
         Nivel${numLevel}
@@ -337,18 +400,18 @@ function openLevel2(level){
         <input id="urlLevel" type="text" size="35" placeholder=" URL da imagem nível${numLevel}" class="caixaDigitavel" />
         <input id="descriptionLevel" type="text" size="35" placeholder=" Descrição do nível${numLevel}" class="caixaDigitavel grande" />
     </div>`
-    
-    if(level.innerHTML !== layout){
+
+    if (level.innerHTML !== layout) {
         level.innerHTML = "";
         level.innerHTML += layout;
         level.removeAttribute('onclick');
     }
 }
 
-function openLevel3(level){
-    
+function openLevel3(level) {
+
     let numLevel = document.querySelector('.nivelNum').innerHTML.replace("Nivel", "");
-    
+
     let layout = `
     <div class="conteinerNiveis nivel3">
         Nivel${numLevel}
@@ -357,7 +420,7 @@ function openLevel3(level){
         <input id="urlLevel" type="text" size="35" placeholder=" URL da imagem nível${numLevel}" class="caixaDigitavel" />
         <input id="descriptionLevel" type="text" size="35" placeholder=" Descrição do nível${numLevel}" class="caixaDigitavel grande" />        
     </div>`
-    
+
     if (level.innerHTML !== layout) {
         level.innerHTML = "";
         level.innerHTML += layout;
@@ -366,7 +429,7 @@ function openLevel3(level){
 }
 
 //validação
-function requestLevels(){
+function requestLevels() {
     let title, hit, url, description, hits = false;
     const titleLevel = document.querySelector('#titleLevel');
     const hitLevel = document.querySelector('#hitLevel');
@@ -374,48 +437,48 @@ function requestLevels(){
     const descriptionLevel = document.querySelector('#descriptionLevel');
 
 
-    if(10 <= titleLevel.value.length){
+    if (10 <= titleLevel.value.length) {
         title = true;
-    }else{
-     alert('O titulo precisa no minimo 10 caracteres');
+    } else {
+        alert('O titulo precisa no minimo 10 caracteres');
     }
 
-    if((hitLevel.value >= 0) && (hitLevel.value <= 100)){
+    if ((hitLevel.value >= 0) && (hitLevel.value <= 100)) {
         hit = true;
-    }else{
+    } else {
         alert('Insira um número de 0 a 100');
         hitLevel.value = "";
     }
 
-    if(checkURL(urlLevel.value)){
+    if (checkURL(urlLevel.value)) {
         url = true
-    }else{
+    } else {
         alert('Insira uma URL válida');
         urlLevel.value = "";
     }
 
-    if(30 <= descriptionLevel.value.length){
+    if (30 <= descriptionLevel.value.length) {
         description = true;
-    }else{
+    } else {
         alert('A descrição precisa ter no minimo 30 caracteres');
         descriptionLevel.value = "";
     }
 
-    if(nNiveis == 2){
+    if (nNiveis == 2) {
         const nivel1 = document.querySelector('.nivel1');
         const nivel2 = document.querySelector('.nivel2');
-    
+
         const hit1 = nivel1.querySelector('#hitLevel');
         const hit2 = nivel2.querySelector('#hitLevel');
 
-        if ((hit1.value == 0) || (hit2.value == 0)){
+        if ((hit1.value == 0) || (hit2.value == 0)) {
             hits = true;
-        }else{
+        } else {
             alert("Em um dos niveis é obrigatorio que a % de acertos seja 0")
         }
     }
 
-    if(nNiveis == 3){
+    if (nNiveis == 3) {
         const nivel1 = document.querySelector('.nivel1');
         const nivel2 = document.querySelector('.nivel2');
         const nivel3 = document.querySelector('.nivel3');
@@ -424,21 +487,21 @@ function requestLevels(){
         const hit2 = nivel2.querySelector('#hitLevel');
         const hit3 = nivel3.querySelector('#hitLevel');
 
-        if ((hit1.value == 0) || (hit2.value == 0) || (hit3.value == 0)){
+        if ((hit1.value == 0) || (hit2.value == 0) || (hit3.value == 0)) {
             hits = true;
-        }else{
+        } else {
             alert("Em um dos niveis é obrigatorio que a % de acertos seja 0")
         }
     }
 
 
-    if(title == true && hit == true && url == true && description == true && hits == true){
+    if (title == true && hit == true && url == true && description == true && hits == true) {
         endQuizz();
     }
 
 }
 
-function endQuizz(){
+function endQuizz() {
     const finishQuizz = document.querySelector('.end');
     const telaNiveis = document.querySelector('.levels');
 
@@ -446,11 +509,16 @@ function endQuizz(){
     telaNiveis.classList.add('desativar');
 }
 
-function checkURL(string){
+function checkURL(string) {
     try {
         let url = new URL(string)
         return true
-      } catch(err) {
-          return false
-      }
+    } catch (err) {
+        return false
+    }
+}
+
+// CRIAÇÃO DO OBJETO PARAR ENVIO
+function oi(oi){
+
 }
